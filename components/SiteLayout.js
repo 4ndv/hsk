e30c38ee7e38b29550dc80e0c3037503442f3a81
Andrey Viktorov <andreyviktorov@outlook.com>
2020-04-23 03:58:57 +0700

    Added source code link

--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -19,7 +19,9 @@ const SiteLayout = ({ children }) => {
         <footer>
           Made by&nbsp;
           <a href="https://lynx.pink" target="_blank" rel="noopener">lynx</a>
-          , 2020
+          , 2020.
+          {' '}
+          <a href="https://github.com/4ndv/hsk" target="_blank" rel="noopener">Source code and issues reports.</a>
         </footer>
       </Container>
     </div>
