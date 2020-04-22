4154b474c3e7b0380dc1dd25a0bb5760d8c12f15
Andrey Viktorov <andreyviktorov@outlook.com>
2020-04-23 02:25:05 +0700

    App wrapper

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import '~/styles/main.scss'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+MyApp.propTypes = {
+  Component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
+  pageProps: PropTypes.object.isRequired
+}
+
+export default MyApp
